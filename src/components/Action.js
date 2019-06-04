caa6d501d0ef19c155300af490deb2a56fbc03bf
Jorge Chiquín Valderrama <jorgechiquinv@gmail.com>
2019-06-04 09:46:13 +0000

    Page for notification action.

--- src/components/Action.js
+++ src/components/Action.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import { Typography, Grid } from '@material-ui/core';
+import { Link } from "react-router-dom";
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    marginTop: theme.spacing(5),
+  }
+}));
 
 function Action() {
+  
+  const classes = useStyles();
+  
   return (
     <div>
-      Notification clicked
+      <Grid
+        direction="column"
+        container
+        justify="center"
+        alignItems="center"
+        className={classes.root}
+      >
+        <Typography variant="h4">
+          Congrats!
+        </Typography>
+        <Typography variant="h5" component="h3">
+          You has clicked a notification.
+        </Typography>
+        
+        <Typography component="p">
+          <Link to="/">
+            Go home
+          </Link>
+        </Typography>
+        
+      </Grid>
     </div>
   );
 }
